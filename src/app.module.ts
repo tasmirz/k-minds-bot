f49ad6f8b18c38f40ffe6f91ee2cc122cc95abd6
tasmirz <zihad2107071@stud.kuet.ac.bd>
2025-11-01 23:16:07 +0600

    healthcheck requirement

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { DiscordModule } from './discord/discord.module'
+import { AppController } from './app.controller'
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(
-      process.env.MONGO_URI || 'mongodb://localhost:27017/kminds',
-    ),
-    DiscordModule,
-  ],
-  controllers: [],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost:27017/kminds'), DiscordModule],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
