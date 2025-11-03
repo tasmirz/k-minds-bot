e752eb856879a24f432a60c27a21948d18f5e939
tasmirz <zihad2107071@stud.kuet.ac.bd>
2025-11-03 09:25:18 +0600

    feat: add docker-compose configuration for app service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { DiscordModule } from './discord/discord.module'
 import { AppController } from './app.controller'
+import { AppService } from './app.service'
 
 @Module({
   imports: [MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost:27017/kminds'), DiscordModule],
   controllers: [AppController],
-  providers: [],
+  providers: [AppService],
 })
 export class AppModule {}
